14f8b1904ae12f68d669a2c9c3a4eb29b3e195dd
Marlene Abigail Salas Guerrero <marlene.salas2218@gmail.com>
2024-09-25 18:11:39 -0600

    Agregar archivos de proyecto.

---
+++ PokemonPWA/Pages/Datos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PokemonPWA.Pages
+{
+    public class DatosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PokemonPWA/wwwroot/serviceworker.js
@@ -0,0 +1,45 @@
+﻿// service-worker.js
+
+let urls = [
+    "/",
+    "/index",
+    "/datos",
+    "/estilos.css",
+    "/imgs/icono.png",
+    "/imgs/icono-128.png",
+    "/imgs/icono-512.png",
+    "/imgs/pokemon.png",
+];
+
+let cacheName = "pokemonCachev1";
+
+async function precache() {
+    let cache = await caches.open(cacheName);
+    cache.addAll(urls);
+}
+
+//precache
+self.addEventListener("install", function (e) {
+    e.waitUntil(precache());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(getFromCache(event.request));
+});
+
+async function getFromCache(url) {
+    try {
+
+        let cache = await caches.open(cacheName);
+        let response = await cache.match(url);
+        if (response) {
+            return response
+        } else {
+            let respuesta = await fetch(url);
+            cache.put(url, respuesta.clone());
+            return respuesta;
+        }
+    } catch (x) {
+        console.log(x);
+    }
+}
